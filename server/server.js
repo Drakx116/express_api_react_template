890f6b5843cf37b3d910764d19518c1c85f0207e
Drakx116 <alexandre2.guyot@gmail.com>
2020-03-21 11:41:14 +0100

    Add dotenv config

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+const ENV = require('dotenv').config().parsed;
 const express = require('express');
 
 const app = express();
 const port = 5000;
 
 app.listen(port,
-    () => console.log(`Server is running on localhost:${port}`)
+    () => console.log(`Server is running on ${ ENV.HOST }:${ ENV.PORT }`)
 );
