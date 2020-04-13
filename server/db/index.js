9eb4832befd263ed03ddb531813c5481350dc402
Drakx116 <alexandre2.guyot@gmail.com>
2020-04-13 15:20:52 +0200

    Init mongoose connection

---
+++ server/db/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const ENV = require('dotenv').config().parsed;
+
+const connect = (req, res, next) => {
+    mongoose.connect(`${ ENV.DB_CONNECTION_STRING }`, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then( ()  => next() )
+        .catch( () => { return res.status(500).json({ 'message': 'Error while accessing the database.' }) }  );
+};
+
+module.exports = connect;
--- server/server.js
+++ server/server.js
@@ -4,11 +4,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const db = require('./db');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet());
+app.use(db);
 
 app.listen(ENV.PORT,
     () => console.log(`Server is running on ${ ENV.HOST }:${ ENV.PORT }`)
