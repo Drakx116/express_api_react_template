61a377166bcdfb528c1c0f81756c6b683c6ea1af
Drakx116 <alexandre2.guyot@gmail.com>
2020-04-13 15:11:16 +0200

    Add base middlewares

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const ENV = require('dotenv').config().parsed;
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
 
 app.listen(ENV.PORT,
     () => console.log(`Server is running on ${ ENV.HOST }:${ ENV.PORT }`)
